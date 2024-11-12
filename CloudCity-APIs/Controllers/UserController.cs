3b9b1047d98dfd94002a885e85bfb8c7400a0d0e
Danwiel <danwiel13@hotmail.com>
2024-11-12 09:56:26 -0600

    ahora se usara el rama "dev"  y se ha completado los requisitos, que pida, guarde y muestre la informacion ingresada, ya fue probada en postman y realiza lo pedido

---
+++ CloudCity-APIs/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using CloudCity_APIs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public UsersController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+    }
+}
